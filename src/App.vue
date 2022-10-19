ba44cb32cb3a53d6ae5fd1cedd5a92c8129586f7
jmontalvans <dmontalvan.santisteban@gmail.com>
2022-10-19 00:01:51 -0500

    Vue3app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+  <img src="./assets/logo.svg" alt="" height="120" width="120">
+  <h1>Hola mundo de Vue</h1>
+  <hr />
+
+  <h2>Contador: 0</h2>
+  <button>+1</button>
+  <button>+1</button>
+  <button>Reset</button>
+
+
+</template>
